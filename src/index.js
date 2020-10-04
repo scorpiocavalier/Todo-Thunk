bbfd7c609c1265990b4a369956ab1894f7ea3028
Arturo Chen <art.can.fly@gmail.com>
2020-10-03 22:53:49 -0400

    + Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { configureStore } from './redux/store'
+
+const store = configureStore()
 
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from 'redux'
+
+const reducers = {
+
+}
+
+const rootReducer = combineReducers(reducers)
+
+export const configureStore = () => createStore(rootReducer)
