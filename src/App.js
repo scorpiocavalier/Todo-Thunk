db13748c94b7e0a8eef2071caf332bbd704704ac
Arturo Chen <art.can.fly@gmail.com>
2020-10-03 22:38:18 -0400

    + React-Hot-Loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
-export default function App() {
+const App = () => {
   return (
     <div>
       <h1>Hello</h1>
     </div>
   )
 }
+
+export default hot(module)(App)
