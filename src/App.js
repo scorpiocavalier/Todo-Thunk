902c5050cf6988adb298cea70538c2c4064c849b
Arturo Chen <art.can.fly@gmail.com>
2020-10-03 22:49:37 -0400

    * render NewTodoForm and TodoList in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import TodoList from './todos/TodoList'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <TodoList />
     </div>
   )
 }
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import NewTodoForm from './NewTodoForm'
 import TodoListItem from './TodoListItem'
 
-export const TodoList = ({ todos }) => {
+export const TodoList = ({ todos = [] }) => {
   return (
     <div>
+      <NewTodoForm />
       {todos.map(todo => <TodoListItem todo={ todo } />) }
     </div>
   )
