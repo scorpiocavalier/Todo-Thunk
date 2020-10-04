589a72e4e358200cf286dca42b57d6ac2aa3fe23
Arturo Chen <art.can.fly@gmail.com>
2020-10-04 00:05:48 -0400

    + Redux Persist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 
 import App from './App'
 import { configureStore } from './redux/store'
 
 const store = configureStore()
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={ store }>
-    <App />
+    <PersistGate
+      loading={ <div>Loading...</div> }
+      persistor={ persistor }
+    >
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { createStore, combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+
 import { todos } from './reducers'
+
+const persistConfig = {
+  key: 'root',
+  storage, // web local storage
+  stateReconciler: autoMergeLevel2
+}
 
 const reducers = {
   todos
 }
 
 const rootReducer = combineReducers(reducers)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const configureStore = () => createStore(rootReducer)
+export const configureStore = () => createStore(persistedReducer)
